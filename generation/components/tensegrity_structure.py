8d5c5e17b88f0d5e641d0f8bf7d2c1ce835342b7
Jeremy Wright <wrightjmf@gmail.com>
2021-01-09 11:20:36 -0500

    Added the rest of the strings to the tensegrity structure

--- generation/components/tensegrity_structure.py
+++ generation/components/tensegrity_structure.py
@@ -77,36 +77,51 @@ class TensegrityStructure:
         # Step through all the q and p units and place the nodes for them
         for i in range(0, self.q):
             for k in range(0, self.p):
-                # Add the current node
-                N_1_2 = N(i, k, self.R, self.r, self.p, self.q).get()
-                self.structure.add(Node(N_1_2[0], 1, self.r, simplified=True).get(), color=cq.Color(1, 1, 1, 1))
-                self.structure.add(Node(N_1_2[0], 2, self.r, simplified=True).get(), color=cq.Color(1, 1, 1, 1))
-
                 # Get the indexes of the next and previous strips
-                i_adj = i + 1
-                k_adj = k + 1
-                k_m_adj = k - 1
+                ip = i + 1
+                im = i - 1
+                kp = k + 1
+                km = k - 1
 
                 # If we have looped around, reset the adjacent i and k indices for the boundary conditions
-                if i_adj > self.q:
-                    i_adj = 0
-                if k_adj > self.p:
-                    k_adj = 0
-                if k_m_adj < 0:
-                    k_m_adj = self.p - 1
+                if ip > self.q:
+                    ip = 0
+                if im < 0:
+                    im = self.q - 1
+                if kp > self.p:
+                    kp = 0
+                if km < 0:
+                    km = self.p - 1
 
                 # Find the adjacent bar node connections for the current ik unit
-                N_1_2_plus = N(i_adj, k_adj, self.R, self.r, self.p, self.q).get()
-                N_1_2_minus = N(i_adj, k_m_adj, self.R, self.r, self.p, self.q).get()
-                N_ip_k = N(i_adj, k, self.R, self.r, self.p, self.q).get()
+                # ip = i + 1
+                # kp = k + 1
+                # im = i - 1
+                # km = k - 1
+                N_i_k = N(i, k, self.R, self.r, self.p, self.q).get()
+                N_ip_kp = N(ip, kp, self.R, self.r, self.p, self.q).get()
+                N_ip_km = N(ip, km, self.R, self.r, self.p, self.q).get()
+                N_ip_k = N(ip, k, self.R, self.r, self.p, self.q).get()
+                N_i_kp = N(i, kp, self.R, self.r, self.p, self.q).get()
+                N_im_k = N(im, k, self.R, self.r, self.p, self.q).get()
+                N_im_km = N(im, km, self.R, self.r, self.p, self.q).get()
+
+                # Add the current node
+                self.structure.add(Node(N_i_k[0], 1, self.r, simplified=self.simplified).get(), color=cq.Color(1, 1, 1, 1))
+                self.structure.add(Node(N_i_k[0], 2, self.r, simplified=self.simplified).get(), color=cq.Color(1, 1, 1, 1))
 
                 # Create a bar between the n1 node of the current unit to the n1 node of the i+1,k+1 unit
-                self.structure.add(TBar(N_1_2[0], N_1_2_plus[0], 1, self.r).get(), color=cq.Color(0, 0, 1))
-                self.structure.add(TBar(N_1_2[0], N_1_2_minus[0], 2, self.r).get(), color=cq.Color(0, 0, 1))
+                self.structure.add(TBar(N_i_k[0], N_ip_kp[0], 1, self.r).get(), color=cq.Color(0, 0, 1))
+                self.structure.add(TBar(N_i_k[0], N_ip_km[0], 2, self.r).get(), color=cq.Color(0, 0, 1))
 
                 # Add the strings between the appropriate nodes
-                self.structure.add(TString(N_1_2[0], N_1_2[0], 1, 2, self.r).get(), color=cq.Color(1, 0, 0))
-                self.structure.add(TString(N_1_2[0], N_ip_k[0], 2, 1, self.r).get(), color=cq.Color(1, 0, 0))
-                self.structure.add(TString(N_1_2[0], N_ip_k[0], 1, 1, self.r).get(), color=cq.Color(1, 0, 0))
+                self.structure.add(TString(N_i_k[0], N_i_k[0], 1, 2, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n1 to N(i,k) n2
+                self.structure.add(TString(N_i_k[0], N_ip_k[0], 2, 1, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n2 to N(i+1,k) n1
+                self.structure.add(TString(N_i_k[0], N_im_km[0], 1, 2, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n1 to N(i-1,k-1) n2
+                self.structure.add(TString(N_i_k[0], N_i_kp[0], 2, 1, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n2 to N(i,k+1) n1
+                self.structure.add(TString(N_i_k[0], N_i_kp[0], 2, 2, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n2 to N(i,k+1) n2
+                self.structure.add(TString(N_i_k[0], N_ip_k[0], 1, 1, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n1 to N(i+1,k) n1
+                self.structure.add(TString(N_i_k[0], N_i_kp[0], 1, 1, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n1 to N(i,k+1) n1
+                self.structure.add(TString(N_i_k[0], N_ip_k[0], 2, 2, self.r).get(), color=cq.Color(1, 0, 0)) # N(i,k) n2 to N(i+1,k) n2
 
         return self.structure
